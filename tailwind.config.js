629a776111df87f0c78782432c3db8e5d4c7c847
John Durance <j.durance@1xinternet.de>
2022-05-28 15:01:28 +0200

    Add raw content

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,16 @@
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ["./*.html"],
   theme: {
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: '1rem',
+        sm: '2rem',
+        lg: '4rem',
+        xl: '5rem',
+        '2xl': '6rem',
+      },
+    },
     extend: {},
   },
   plugins: [],
